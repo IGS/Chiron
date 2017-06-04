d92dbf46f7bdc2f4ed27851a0843abc7ba17933d
jorvis <jorvis@gmail.com>
2017-06-04 01:24:38 -0500

    Removed now-defunct rebuild script for omnibus image

--- docker/rebuild_docker_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker rmi -f jorvis/hmp-cloud-pilot
-docker build --no-cache -t jorvis/hmp-cloud-pilot .
-docker images
-
-echo "If ready for release, run: docker tag <newest tag here> jorvis/hmp-cloud-pilot:latest"
-echo "         docker push jorvis/hmp-cloud-pilot"
