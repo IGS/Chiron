2a36b03fb7d51089581ad02dcc6db2678504b05b
jorvis <jorvis@gmail.com>
2017-06-05 00:10:25 -0500

    Updated QIIME2 to version 2017.5

--- docker/chiron-qiime2/build.sh
+++ docker/chiron-qiime2/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker rmi -f jorvis/chiron-qiime2
-docker build --no-cache -t jorvis/chiron-qiime2 .
+docker rmi -f umigs/chiron-qiime2
+docker build --no-cache -t umigs/chiron-qiime2 .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> jorvis/chiron-qiime2:latest"
-echo "  docker push jorvis/chiron-qiime2"
+echo "  docker tag <newest tag here> umigs/chiron-qiime2:latest"
+echo "  docker push umigs/chiron-qiime2"
