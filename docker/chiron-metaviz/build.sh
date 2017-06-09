fe13683e5e9911c2aed7779121b5f37342f48218
Justin Wagner <jmwagner@umd.edu>
2017-06-09 16:25:45 -0400

    Fixes for metavizr

--- docker/chiron-metaviz/build.sh
+++ docker/chiron-metaviz/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker rmi -f umigs/chiron-metaviz
-docker build --no-cache -t umigs/chiron-metaviz .
-docker images
+sudo docker rmi -f umigs/chiron-metaviz
+sudo docker build --no-cache -t umigs/chiron-metaviz .
+sudo docker images
 
 echo "If ready for release, run: "
 echo "  docker tag <newest tag here> umigs/chiron-metaviz:latest"
--- docker/chiron-metaviz/setup.R
+++ docker/chiron-metaviz/setup.R
@@ -1,3 +1,6 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite(c("phyloseq", "metagenomeSeq"))
-biocLite("metavizr", dependencies=TRUE)
+#biocLite("metavizr", dependencies=TRUE)
+install.packages("devtools", repos='http://cran.revolutionanalytics.com/')
+library(devtools)
+install_github("epiviz/metavizr", dependencies=TRUE)
