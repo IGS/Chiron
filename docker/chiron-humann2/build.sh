ba594f38d52d8400b0567d47a12a4c06bacbe65e
jorvis <jorvis@gmail.com>
2017-06-13 13:46:48 -0500

    Updated build and Dockerfile for 1.0.0 release

--- docker/chiron-humann2/build.sh
+++ docker/chiron-humann2/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.0.0'
+
 docker rmi -f umigs/chiron-humann2
-docker build --no-cache -t umigs/chiron-humann2 .
+docker build --no-cache -t umigs/chiron-humann2:latest -t umigs/chiron-humann2:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-humann2:latest"
-echo "  docker push umigs/chiron-humann2"
+echo "  docker push umigs/chiron-humann2:latest"
+echo "  docker push umigs/chiron-humann2:${IMAGE_VERSION}"
+
