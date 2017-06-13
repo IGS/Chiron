71c2aa215f752f07a5311a3b7fe2c56489d9f53e
jorvis <jorvis@gmail.com>
2017-06-13 14:03:55 -0500

    Updated build and Dockerfile for 1.0.0 release

--- docker/chiron-metacompass/build.sh
+++ docker/chiron-metacompass/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.0.0'
+
 docker rmi -f umigs/chiron-metacompass
-docker build --no-cache -t umigs/chiron-metacompass .
+docker build --no-cache -t umigs/chiron-metacompass:latest -t umigs/chiron-metacompass:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-metacompass:latest"
-echo "  docker push umigs/chiron-metacompass"
+echo "  docker push umigs/chiron-metacompass:latest"
+echo "  docker push umigs/chiron-metacompass:${IMAGE_VERSION}"
+
