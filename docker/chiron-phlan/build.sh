36f1f4af2fb0a38a3e285113a01a3bcfb26da019
jorvis <jorvis@gmail.com>
2017-06-13 13:34:27 -0500

    Updated build and Dockerfile for 1.0.0 release

--- docker/chiron-phlan/build.sh
+++ docker/chiron-phlan/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.0.0'
+
 docker rmi -f umigs/chiron-phlan
-docker build --no-cache -t umigs/chiron-phlan .
+docker build --no-cache -t umigs/chiron-phlan:latest -t umigs/chiron-phlan:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-phlan:latest"
-echo "  docker push umigs/chiron-phlan"
+echo "  docker push umigs/chiron-phlan:latest"
+echo "  docker push umigs/chiron-phlan:${IMAGE_VERSION}"
