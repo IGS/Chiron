b5a758a8c2172c15e1cefcb60de4c949067bacf0
jorvis <jorvis@gmail.com>
2017-06-13 15:39:22 -0500

    Changed build for core to 1.1.0, R install now uses specific repository

--- docker/chiron-core/build.sh
+++ docker/chiron-core/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.1.0'
+
 docker rmi -f umigs/chiron-core
-docker build --no-cache -t umigs/chiron-core .
+docker build --no-cache -t umigs/chiron-core:latest -t umigs/chiron-core:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-core:latest"
-echo "  docker push umigs/chiron-core"
+echo "  docker push umigs/chiron-core:latest"
+echo "  docker push umigs/chiron-core:${IMAGE_VERSION}"
+
