f81915e0a8e328adc8a1d93fea369fee27399519
Ubuntu <ubuntu@ip-172-31-26-174.ec2.internal>
2017-06-13 14:28:26 -0400

    Core files now reference release 1.0.0

--- docker/chiron-qiime2/build.sh
+++ docker/chiron-qiime2/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.0.0'
+
 docker rmi -f umigs/chiron-qiime2
-docker build --no-cache -t umigs/chiron-qiime2 .
+docker build --no-cache -t umigs/chiron-qiime2:latest -t umigs/chiron-qiime2:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-qiime2:latest"
-echo "  docker push umigs/chiron-qiime2"
+echo "  docker push umigs/chiron-qiime2:latest"
+echo "  docker push umigs/chiron-qiime2:${IMAGE_VERSION}"
