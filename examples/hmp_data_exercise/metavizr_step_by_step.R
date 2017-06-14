77ea253495bb041f894e82fd2b2e7e6d7dde1de5
jmwagner <jmwagner@umd.edu>
2017-06-14 11:50:38 -0400

    Create metavizr_step_by_step.R

---
+++ examples/hmp_data_exercise/metavizr_step_by_step.R
@@ -0,0 +1,21 @@
+
+startMetavizTutorial <- function() {
+	require(httr, quietly=TRUE)
+	public_ipv4 <-  	httr::content(httr::GET("http://169.254.169.254/latest/meta-data/public-ipv4"))
+
+  setMetavizStandalone()
+  startMetavizStandalone(host=public_ipv4, )
+}
+
+download.file("https://github.com/IGS/Chiron/blob/master/docker/chiron-metaviz/workshopData?raw=true" ,destFile = "workshopData")
+
+wgs_16s_compare_obj <- readRDS("workshopData")
+
+app <- startMetavizTutorial()
+
+facetZoom <- app$plot(wgs_16s_compare_obj, datasource_name = "wgs_16s_compare", feature_order = colnames(fData(wgs_16s_compare_obj))[1:6])
+
+## Run this command in the R console after each UI interaction
+## This is an issue with rstudio-server that we are working to resolve
+httpuv::service()
+
