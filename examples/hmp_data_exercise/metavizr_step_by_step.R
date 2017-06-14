9da1cfd53dbb4da060530729716030093a3779b2
jmwagner <jmwagner@umd.edu>
2017-06-14 12:28:26 -0400

    Create metavizr_step_by_step.R

--- examples/hmp_data_exercise/metavizr_step_by_step.R
+++ examples/hmp_data_exercise/metavizr_step_by_step.R
@@ -1,15 +1,16 @@
-
 startMetavizTutorial <- function() {
 	require(httr, quietly=TRUE)
-	public_ipv4 <-  	httr::content(httr::GET("http://169.254.169.254/latest/meta-data/public-ipv4"))
+	public_ipv4 <- httr::content(httr::GET("http://169.254.169.254/latest/meta-data/public-ipv4"))
 
-  setMetavizStandalone()
-  startMetavizStandalone(host=public_ipv4, )
+  	setMetavizStandalone()
+  	startMetavizStandalone(host=public_ipv4)
 }
 
-download.file("https://github.com/IGS/Chiron/blob/master/docker/chiron-metaviz/workshopData?raw=true" ,destFile = "workshopData")
+download.file("https://github.com/IGS/Chiron/blob/master/docker/chiron-metaviz/workshopData?raw=true" ,destfile = "workshopData")
 
 wgs_16s_compare_obj <- readRDS("workshopData")
+
+library(metavizr)
 
 app <- startMetavizTutorial()
 
@@ -18,4 +19,3 @@ facetZoom <- app$plot(wgs_16s_compare_obj, datasource_name = "wgs_16s_compare", 
 ## Run this command in the R console after each UI interaction
 ## This is an issue with rstudio-server that we are working to resolve
 httpuv::service()
-
