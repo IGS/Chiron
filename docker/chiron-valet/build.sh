d5a1a05caeab1697262fa4a200bd1c70bad0009d
jorvis <jorvis@gmail.com>
2017-06-15 13:27:50 -0500

    Manual code fixes need, as reported by Todd, invoking new VALET version

--- docker/chiron-valet/build.sh
+++ docker/chiron-valet/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_VERSION='1.0.3'
+IMAGE_VERSION='1.0.4'
 
 docker rmi -f umigs/chiron-valet
 docker build --no-cache -t umigs/chiron-valet:latest -t umigs/chiron-valet:${IMAGE_VERSION} .
