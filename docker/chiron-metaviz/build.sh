64d92d4e9d7935fa253ebe8d32ed733c229a7d22
jorvis <jorvis@gmail.com>
2017-06-16 10:05:02 -0500

    Updated release of MetaViz to 1.1.0

--- docker/chiron-metaviz/build.sh
+++ docker/chiron-metaviz/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+IMAGE_VERSION='1.1.0'
+
 docker rmi -f umigs/chiron-metaviz
-docker build --no-cache -t umigs/chiron-metaviz .
+docker build --no-cache -t umigs/chiron-metaviz:latest -t umigs/chiron-metaviz:${IMAGE_VERSION} .
 docker images
 
+
 echo "If ready for release, run: "
-echo "  docker tag <newest tag here> umigs/chiron-metaviz:latest"
-echo "  docker push umigs/chiron-metaviz"
+echo "  docker push umigs/chiron-metaviz:latest"
+echo "  docker push umigs/chiron-metaviz:${IMAGE_VERSION}"
