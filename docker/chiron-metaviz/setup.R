acb8b3d468ba7c86c234733c2e17a9e2a6186d50
Justin Wagner <jmwagner@umd.edu>
2017-06-16 10:29:33 -0400

    Dockerfile setup changes for take home exercises

--- docker/chiron-metaviz/setup.R
+++ docker/chiron-metaviz/setup.R
@@ -1,5 +1,9 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite(c("phyloseq", "metagenomeSeq"))
 install.packages("devtools", repos='http://cran.revolutionanalytics.com/')
+install.packages("devtools", repos='http://cran.revolutionanalytics.com/')
+install.packages("tidyverse", repos='http://cran.revolutionanalytics.com/')
+install.packages("RNeo4j", repos='http://cran.revolutionanalytics.com/')
 library(devtools)
-install_github("epiviz/metavizr", dependencies=TRUE)
+install_github("epiviz/metavizr@chiron", dependencies=TRUE)
+
